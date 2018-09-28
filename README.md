9a29e92c54da4aefd19786f3aed9060735c7ad80
claurier <carl.laurier@sword-group.com>
2018-09-28 15:32:33 +0200

    le projet React marche via l'URL http://myccah.claurier.com/

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Fusion entre le projet java https://github.com/szerhusenBC/jwt-spring-security-demo et le projet React react-redux-registration-username-example-master
 
-Le projet React se teste via l'URL http://localhost:8080.
-Actuellement seul le projet React marche [http://localhost:8080/].
+Actuellement seul le projet React marche via l'URL http://myccah.claurier.com/
+http://localhost:8080 ne marche plus
---
+++ client/webpack.config.js
@@ -0,0 +1,50 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var hostname = '0.0.0.0' ;
+var port = 10080;
+
+module.exports = {
+    entry: [
+        `webpack-dev-server/client?http://${hostname}:${port}`,
+
+        './src/index.jsx'
+    ],
+    output: {
+        path: path.resolve('dist'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-3']
+                }
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+    })],
+    devServer: {
+        historyApiFallback: true,
+        disableHostCheck: true,
+        hostname: hostname,
+        port: port
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'http://localhost:4000'
+        })
+    }
+}
